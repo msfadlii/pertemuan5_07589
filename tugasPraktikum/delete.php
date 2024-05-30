24629c573bc505c29b1df791442e6ca471d8ef94
Lavenza <114128985+msfadlii@users.noreply.github.com>
2024-05-30 15:45:48 +0700

    Delete tugasPraktikum directory

--- tugasPraktikum/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-$id = $_GET['id_transaksi'];
-$sql = mysqli_query($koneksi, "DELETE FROM transaksi WHERE id_transaksi='$id'");
-
-header("Location:index.php?pesan=hapus");
-
-?>
--- tugasPraktikum/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $koneksi = mysqli_connect("localhost", "root", "", "pertemuan5_07589");
-    // if(!$koneksi){
-    //     die("Koneksi database gagal: " .mysqli_connect_error());
-    // }else{
-    //     echo "Koneksi database berhasil!";
-    // }
-?>
